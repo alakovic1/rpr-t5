f84cbff79cb19fa2bb4771c5451cbfb23f018ce3
alakovic1 <alakovic1@etf.unsa.ba>
2018-11-14 23:23:04 +0100

    digitron

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -13,64 +13,133 @@ public class Controller {
         prikaz = new SimpleStringProperty("");
     }
 
-    public String getUnos() {
+    public String getUnos1() {
+        return unos1.get();
+    }
+
+    public void setUnos1(String unos1) {
+        this.unos1.set(unos1);
+    }
+
+    public String getOperacija() {
+        return operacija.get();
+    }
+
+    public SimpleStringProperty operacijaProperty() {
+        return operacija;
+    }
+
+    public void setOperacija(String operacija) {
+        this.operacija.set(operacija);
+    }
+
+    public SimpleStringProperty unos1Property() {
+        return unos1;
+    }
+
+    public String getPrikaz() {
         return prikaz.get();
     }
 
-    public void setUnos(String u) {
+    public SimpleStringProperty displejProperty() {
+        return prikaz;
+    }
+
+    public void setPrikaz(String u) {
         this.prikaz.set(u);
     }
 
     public void za0(ActionEvent event) {
-        setUnos("0");
+        setPrikaz("0");
     }
 
     public void za1(ActionEvent event) {
-        setUnos(prikaz.get() + "1");
+        setPrikaz(getPrikaz() + "1");
     }
 
     public void za2(ActionEvent event) {
-        setUnos(prikaz.get() + "2");
+        setPrikaz(getPrikaz() + "2");
     }
 
     public void za3(ActionEvent event) {
-        setUnos(prikaz.get() + "3");
+        setPrikaz(getPrikaz() + "3");
     }
 
     public void za4(ActionEvent event) {
-        setUnos(prikaz.get() + "4");
+        setPrikaz(getPrikaz() + "4");
     }
 
     public void za5(ActionEvent event) {
-        setUnos(prikaz.get() + "5");
+        setPrikaz(getPrikaz() + "5");
     }
 
     public void za6(ActionEvent event) {
-        setUnos(prikaz.get() + "6");
+        setPrikaz(getPrikaz() + "6");
     }
 
     public void za7(ActionEvent event) {
-        setUnos(prikaz.get() + "7");
+        setPrikaz(getPrikaz() + "7");
     }
 
     public void za8(ActionEvent event) {
-        setUnos(prikaz.get() + "8");
+        setPrikaz(getPrikaz() + "8");
     }
 
     public void za9(ActionEvent event) {
-        setUnos(prikaz.get() + "9");
+        setPrikaz(getPrikaz() + "9");
     }
 
     public void zaMinus(ActionEvent event){
-        unos1.set(prikaz.get());
-        prikaz.set("");
-        operacija.set("-");
+        setUnos1(getPrikaz());
+        setPrikaz("");
+        setOperacija("-");
     }
 
     public void zaPlus(ActionEvent event){
-        unos1.set(prikaz.get());
-        prikaz.set("");
-        operacija.set("+");
+        setUnos1(getPrikaz());
+        setPrikaz("");
+        setOperacija("+");
+    }
+
+    public void zaMod(ActionEvent event){
+        setUnos1(getPrikaz());
+        setPrikaz("");
+        setOperacija("%");
+    }
+
+    public void zaDijeljenje(ActionEvent event){
+        setUnos1(getPrikaz());
+        setPrikaz("");
+        setOperacija("/");
+    }
+
+    public void zaMnozenje(ActionEvent event){
+        setUnos1(getPrikaz());
+        setPrikaz("");
+        setOperacija("*");
+    }
+
+    public void zaTacku(ActionEvent event){
+        setPrikaz(getPrikaz()+".");
+    }
+
+    public void zaJednako(ActionEvent event){
+        if(getOperacija().equals("+")){
+            setPrikaz(Double.toString(Double.parseDouble(unos1.get())+Double.parseDouble(getPrikaz())));
+        }
+        else if(getOperacija().equals("-")){
+            setPrikaz(Double.toString(Double.parseDouble(unos1.get())-Double.parseDouble(getPrikaz())));
+        }
+        else if(getOperacija().equals("*")){
+            setPrikaz(Double.toString(Double.parseDouble(unos1.get())*Double.parseDouble(getPrikaz())));
+        }
+        else if(getOperacija().equals("/")) {
+            setPrikaz(Double.toString(Double.parseDouble(unos1.get()) / Double.parseDouble(getPrikaz())));
+        }
+        else if(getOperacija().equals("%")){
+            setPrikaz(Double.toString(Double.parseDouble(unos1.get())+Double.parseDouble(getPrikaz())));
+        }
+        setOperacija("=");
     }
 
 }
