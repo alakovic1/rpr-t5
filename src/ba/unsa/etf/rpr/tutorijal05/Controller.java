5bf3949984fd9d7d39bd86ddf4c30d15664831f6
alakovic1 <alakovic1@etf.unsa.ba>
2018-11-26 21:01:31 +0100

    zavrsen tutorijal, dodani testovi

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -5,141 +5,150 @@ import javafx.event.ActionEvent;
 
 public class Controller {
 
-    private SimpleStringProperty prikaz;
-    private SimpleStringProperty unos1 = new SimpleStringProperty("");;
-    private SimpleStringProperty operacija = new SimpleStringProperty("=");;
+    private enum Operator {ADD, SUB, MUL, DIV, MOD, EQU};
+    private SimpleStringProperty number;
+    private Operator operator = Operator.EQU;
+    private double result = 0;
+    private boolean oClear = false;
+    private boolean temp = false;
 
     public Controller() {
-        prikaz = new SimpleStringProperty("");
+        number = new SimpleStringProperty();
+        number.set("0");
     }
 
-    public String getUnos1() {
-        return unos1.get();
+    public SimpleStringProperty numberProperty() {
+        return number;
     }
 
-    public void setUnos1(String unos1) {
-        this.unos1.set(unos1);
+    public String getNumber() {
+        return number.get();
     }
 
-    public String getOperacija() {
-        return operacija.get();
-    }
-
-    public SimpleStringProperty operacijaProperty() {
-        return operacija;
-    }
-
-    public void setOperacija(String operacija) {
-        this.operacija.set(operacija);
-    }
-
-    public SimpleStringProperty unos1Property() {
-        return unos1;
-    }
-
-    public String getPrikaz() {
-        return prikaz.get();
-    }
-
-    public SimpleStringProperty displejProperty() {
-        return prikaz;
-    }
-
-    public void setPrikaz(String u) {
-        this.prikaz.set(u);
-    }
-
-    public void za0(ActionEvent event) {
-        setPrikaz("0");
-    }
-
-    public void za1(ActionEvent event) {
-        setPrikaz(getPrikaz() + "1");
-    }
-
-    public void za2(ActionEvent event) {
-        setPrikaz(getPrikaz() + "2");
-    }
-
-    public void za3(ActionEvent event) {
-        setPrikaz(getPrikaz() + "3");
-    }
-
-    public void za4(ActionEvent event) {
-        setPrikaz(getPrikaz() + "4");
-    }
-
-    public void za5(ActionEvent event) {
-        setPrikaz(getPrikaz() + "5");
-    }
-
-    public void za6(ActionEvent event) {
-        setPrikaz(getPrikaz() + "6");
-    }
-
-    public void za7(ActionEvent event) {
-        setPrikaz(getPrikaz() + "7");
-    }
-
-    public void za8(ActionEvent event) {
-        setPrikaz(getPrikaz() + "8");
-    }
-
-    public void za9(ActionEvent event) {
-        setPrikaz(getPrikaz() + "9");
-    }
-
-    public void zaMinus(ActionEvent event){
-        setUnos1(getPrikaz());
-        setPrikaz("");
-        setOperacija("-");
-    }
-
-    public void zaPlus(ActionEvent event){
-        setUnos1(getPrikaz());
-        setPrikaz("");
-        setOperacija("+");
-    }
-
-    public void zaMod(ActionEvent event){
-        setUnos1(getPrikaz());
-        setPrikaz("");
-        setOperacija("%");
-    }
-
-    public void zaDijeljenje(ActionEvent event){
-        setUnos1(getPrikaz());
-        setPrikaz("");
-        setOperacija("/");
-    }
-
-    public void zaMnozenje(ActionEvent event){
-        setUnos1(getPrikaz());
-        setPrikaz("");
-        setOperacija("*");
-    }
-
-    public void zaTacku(ActionEvent event){
-        setPrikaz(getPrikaz()+".");
-    }
-
-    public void zaJednako(ActionEvent event){
-        if(getOperacija().equals("+")){
-            setPrikaz(Double.toString(Double.parseDouble(getUnos1())+Double.parseDouble(getPrikaz())));
+    private void buildNumber(String num) {
+        if(oClear) {
+            numberProperty().setValue("");
+            oClear = false;
         }
-        else if(getOperacija().equals("-")){
-            setPrikaz(Double.toString(Double.parseDouble(getUnos1())-Double.parseDouble(getPrikaz())));
+
+        if((getNumber().equals("0") && !num.equals("."))) {
+            numberProperty().setValue("");
+
         }
-        else if(getOperacija().equals("*")){
-            setPrikaz(Double.toString(Double.parseDouble(getUnos1())*Double.parseDouble(getPrikaz())));
+        temp=true;
+        numberProperty().setValue(getNumber().concat(num));
+    }
+
+    private double parseNumber(String num) {
+        return Double.parseDouble(num);
+    }
+
+    private void operate(Operator o) {
+        double operand = parseNumber(getNumber());
+        if(temp) {
+            switch (operator) {
+                case EQU: {
+                    result = operand;
+                    temp = false;
+                    break;
+                }
+                case ADD: {
+                    result += operand;
+                    temp = false;
+                    break;
+                }
+                case SUB: {
+                    result -= operand;
+                    temp = false;
+                    break;
+                }
+                case MUL: {
+                    result *= operand;
+                    temp = false;
+                    break;
+                }
+                case DIV: {
+                    result /= operand;
+                    temp = false;
+                    break;
+                }
+                case MOD: {
+                    result %= operand;
+                    temp = false;
+                    break;
+                }
+            }
         }
-        else if(getOperacija().equals("/")) {
-            setPrikaz(Double.toString(Double.parseDouble(getUnos1()) / Double.parseDouble(getPrikaz())));
-        }
-        else if(getOperacija().equals("%")){
-            setPrikaz(Double.toString(Double.parseDouble(getUnos1())%Double.parseDouble(getPrikaz())));
-        }
-        setOperacija("=");
+        numberProperty().setValue("" + result);
+        oClear = true;
+        operator = o;
+    }
+
+    public void za0(ActionEvent actionEvent) {
+        buildNumber("0");
+    }
+
+    public void za1(ActionEvent actionEvent) {
+        buildNumber("1");
+    }
+
+    public void za2(ActionEvent actionEvent) {
+        buildNumber("2");
+    }
+
+    public void za3(ActionEvent actionEvent) {
+        buildNumber("3");
+    }
+
+    public void za4(ActionEvent actionEvent) { buildNumber("4"); }
+
+    public void za5(ActionEvent actionEvent) {
+        buildNumber("5");
+    }
+
+    public void za6(ActionEvent actionEvent) {
+        buildNumber("6");
+    }
+
+    public void za7(ActionEvent actionEvent) {
+        buildNumber("7");
+    }
+
+    public void za8(ActionEvent actionEvent) {
+        buildNumber("8");
+    }
+
+    public void za9(ActionEvent actionEvent) {
+        buildNumber("9");
+    }
+
+    public void zaTacku(ActionEvent actionEvent) {
+        if(!numberProperty().get().contains(".")) buildNumber(".");
+    }
+
+    public void zaPlus(ActionEvent actionEvent) {
+
+        operate(Operator.ADD);
+    }
+
+    public void zaMinus(ActionEvent actionEvent) {
+        operate(Operator.SUB);
+    }
+
+    public void zaMnozenje(ActionEvent actionEvent) {
+        operate(Operator.MUL);
+    }
+
+    public void zaDijeljenje(ActionEvent actionEvent) {
+        operate(Operator.DIV);
+    }
+
+    public void zaModul(ActionEvent actionEvent) {
+        operate(Operator.MOD);
+    }
+
+    public void zaJednako(ActionEvent actionEvent) {
+        operate(Operator.EQU);
     }
 
 }
